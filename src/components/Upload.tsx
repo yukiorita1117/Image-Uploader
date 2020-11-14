ecbc808aff87d3125cd2094e43d993643b298ed3
折田幸夫 <3204704@gmail.com>
2020-11-14 20:14:46 +0900

    create components

---
+++ src/components/Upload.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 344px;
+  padding: 0 16px;
+`;
+
+type Props = {
+  className?: string;
+};
+
+const Upload: React.FC<Props> = ({ className }: Props) => {
+  return <Root className={className}></Root>;
+};
+export default Upload;
